ad6a3e2016871b1e7a338579c8c64d01c4956804
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2022-11-22 11:30:35 -0800

    demo11

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,25 @@
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+        console.log(currentUser);
+
+        // the following functions are always called when someone is logged in
+        read_display_Quote();
+        insertName();
+        populateCardsDynamically();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
 function read_display_Quote(){
     //console.log("inside the function")
 
     //get into the right collection
-    db.collection("quotes").doc("tuesday")
+    db.collection("quotes").doc("tueday")
     .onSnapshot(function(tuesdayDoc) {
         //console.log(tuesdayDoc.data());
         document.getElementById("quote-goes-here").innerHTML=tuesdayDoc.data().quote;
@@ -10,24 +27,16 @@ function read_display_Quote(){
 }
 read_display_Quote();
 
-function insertName(){
-// to check if the user is logged in:
- firebase.auth().onAuthStateChanged(user =>{
-     if (user){
-         console.log(user.uid); // let me to know who is the user that logged in to get the UID
-        currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-        currentUser.get().then(userDoc=>{
-            //get the user name
-            var user_Name= userDoc.data().name;
-            console.log(user_Name);
-            $("#name-goes-here").text(user_Name); //jquery
-            // document.getElementByID("name-goes-here").innetText=user_Name;
-        })    
-    }
-
- })
+// Insert name function using the global variable "currentUser"
+function insertName() {
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        $("#name-goes-here").text(user_Name); //jquery
+        // document.getElementByID("name-goes-here").innetText=user_Name;
+    })
 }
-insertName();
 
 function writeHikes() {
     //define a variable for the collection you want to create in Firestore to populate data
@@ -35,33 +44,33 @@ function writeHikes() {
 
     hikesRef.add({
         code: "BBY01",
-        name: "Burnaby Lake Park Trail",    //replace with your own city?
+        name: "Burnaby Lake Park Trail", //replace with your own city?
         city: "Burnaby",
         province: "BC",
         level: "easy",
-        length: "10",
-        details: "Elmo goes here regularly",
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        length: 10,          //number value
+        length_time: 60,     //number value
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
     });
     hikesRef.add({
         code: "AM01",
-        name: "Buntzen Lake Trail Trail",    //replace with your own city?
+        name: "Buntzen Lake Trail", //replace with your own city?
         city: "Anmore",
         province: "BC",
         level: "moderate",
-        length: "10.5",
-        details: "Elmo goes here regularly",
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        length: 10.5,      //number value
+        length_time: 80,   //number value
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
     });
     hikesRef.add({
         code: "NV01",
-        name: "Mount Seymoure Trail",    //replace with your own city?
+        name: "Mount Seymour Trail", //replace with your own city?
         city: "North Vancouver",
         province: "BC",
         level: "hard",
-        length: "8.2",
-        details: "Elmo goes here regularly",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
+        length: 8.2,        //number value
+        length_time: 120,   //number value
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2022"))
     });
 }
 
@@ -69,7 +78,7 @@ function populateCardsDynamically() {
     let hikeCardTemplate = document.getElementById("hikeCardTemplate");
     let hikeCardGroup = document.getElementById("hikeCardGroup");
 
-    db.collection("hikes").get()
+    db.collection("hikes").orderBy("length").limit(2).get()
         .then(allHikes => {
             allHikes.forEach(doc => {
                 var hikeName = doc.data().name; //gets the name field
@@ -79,14 +88,32 @@ function populateCardsDynamically() {
                 testHikeCard.querySelector('.card-title').innerHTML = hikeName;     //equiv getElementByClassName
                 testHikeCard.querySelector('.card-length').innerHTML = hikeLength;  //equiv getElementByClassName
                 testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);//equiv getElementByTagName
+                testHikeCard.querySelector('i').id = 'save-' + hikeID; //assign specific ID to the card that was clicked
+                testHikeCard.querySelector('i').onclick = () => saveBookmark(hikeID); // this will call a function upon clicking on the icon
                 testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;   //equiv getElementByTagName
-                hikeCardGroup.appendChild(testHikeCard);
+                testHikeCard.querySelector('.get-details').href = "eachHike.html?hikeName=" + hikeName + "&id=" + hikeID;
+                hikeCardGroup.appendChild(testHikeCard);           
             })
 
         })
 }
-populateCardsDynamically();
+//populateCardsDynamically();
 
 function setHikeData(id) {
     localStorage.setItem('hikeID', id);
+}
+
+function saveBookmark(id) {
+    currentUser.set({
+        bookmarks: firebase.firestore.FieldValue.arrayUnion(id)
+    }, {
+        merge: true
+    })
+        .then(function () {
+            console.log("bookmark has been saved for: " + currentUser);
+            var iconID = 'save-' + id;
+            //console.log(iconID);
+            //this is to change the icon of the hike that was saved to "filled"
+            document.getElementById(iconID).innerText = 'bookmark';
+        });
 }
